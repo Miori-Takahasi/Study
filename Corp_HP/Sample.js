8dfab0b2eb3c107f5cc034e7fb53025ec2b0ab5b
Miori <tkhs21202@gmail.com>
2021-02-14 17:17:18 +0900

    jqueryでメッセージに動きをつけました。

--- Corp_HP/Sample.js
+++
@@ -1,6 +0,0 @@
-console.log('Hello!');
-console.log(10 + 5);
-console.log(10 - 5);
-
-console.log(10 * 5);
-console.log(10 / 5);
---
+++ Corp_HP/motion.js
@@ -0,0 +1,6 @@
+
+$(function() {
+
+	$('#message').hide().fadeIn(1700);
+
+});
