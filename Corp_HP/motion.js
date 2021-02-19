1fff0d0100599a32e37c8e1679a36275ce8fe48e
Miori-Takahasi <tkhs21202@gmail.com>
2021-02-19 21:02:03 +0900

    開閉メニュー
    試行錯誤

--- Corp_HP/motion.js
+++ Corp_HP/motion.js
@@ -2,5 +2,14 @@
 $(function() {
 
 	$('#message').hide().fadeIn(1700);
+	
+	$('.header-icon').on('click', function() {
+		$(this).toggleClass('isClose');
+		if($(this).hasClass('isClose')) {
+			$(this).attr('src','images/ham_close.svg');
+		} else {
+			$(this).attr('src', 'images/hamburger.svg');
+		}
+	});
 
 });
